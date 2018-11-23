f0bc54913d9c2805532809976a536d5194acf251
Marco Antonio Steck Filho <steckmarco@outlook.com>
2018-11-23 02:23:52 -0200

    Fixed misspeling

--- src/main/communicator.hpp
+++ src/main/communicator.hpp
@@ -4,13 +4,15 @@
 #include "Arduino.h"
 
 class Communicator {
+  private:
+    void serialFlush();
 	public:
 		void init();
 		bool authenticate(String id);
 		void addConsumed(String id, int milliliter);
 		String getNameByUUID(String id);
 		String getMlByUUID(String id);
-		bool inMantenance();
+		bool inMaintenance();
 		bool addingUser();
 		void sendRawUUID(String id);
 };
