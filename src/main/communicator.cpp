5cab8ddfdc8c8740d363a8c4058f16d21750a839
Marco Antonio Steck Filho <steckmarco@outlook.com>
2018-11-23 02:22:37 -0200

    Fixed availability bugs

--- src/main/communicator.cpp
+++ src/main/communicator.cpp
@@ -5,30 +5,43 @@ void Communicator::init() {
   Serial.setTimeout(1000);
 }
 
+void Communicator::serialFlush(){
+  while(Serial.available() > 0) {
+    char t = Serial.read();
+  }
+}
+
 bool Communicator::authenticate(String id) {
   Serial.print("check_UUID," + id + "\n");
+  while (!Serial.available());
   if (Serial.read() == 'y') {
     // Authorized
+    this->serialFlush();
     return true;
   }
+  this->serialFlush();
   return false;
 }
 
 void Communicator::sendRawUUID(String id) {
-  Serial.print(id + "\n");
+  Serial.print("raw_UUID," + id + "\n");
+  this->serialFlush();
 }
 
 void Communicator::addConsumed(String id, int milliliter) {
     Serial.print("add_ml," + id + "," + String(milliliter) + "\n");
+    this->serialFlush();
 }
 
 String Communicator::getNameByUUID(String id) {
     Serial.print("get_name," + id + "\n");
 	String name = "";
 	char c = '\0';
+  while (!Serial.available());
 	while ((c = Serial.read()) != '\n') {
 		name += c;
 	}
+  this->serialFlush();
 	return name;
 }
 
@@ -36,9 +49,11 @@ String Communicator::getMlByUUID(String id) {
     Serial.print("get_ml," + id + "\n");
 	String ml = "";
 	char c = '\0';
+  while (!Serial.available());
 	while ((c = Serial.read()) != '\n') {
 		ml += c;
 	}
+  this->serialFlush();
 	if (ml.length() > 0) {
 		return ml.toInt();
 	} else {
@@ -46,18 +61,24 @@ String Communicator::getMlByUUID(String id) {
 	}
 }
 
-bool Communicator::inMantenance() {
-  Serial.print("check_maintenance\n");
+bool Communicator::inMaintenance() {
+  Serial.print("check_maintenance,\n");
+  while (!Serial.available());
   if (Serial.read() == 'y') {
+    this->serialFlush();
     return true;
   }
+  this->serialFlush();
   return false;
 }
 
 bool Communicator::addingUser() {
-  Serial.print("check_adding_user\n");
+  Serial.print("check_adding_user,\n");
+  while (!Serial.available());
   if (Serial.read() == 'y') {
+    this->serialFlush();
     return true;
   }
+  this->serialFlush();
   return false;
 }
